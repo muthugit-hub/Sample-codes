486c641f96b3d7346bd4eb9f84c16662b43992cc
muthugit-hub <yourslittleheartmarimuthu@gmail.com>
2023-06-07 22:26:31 +0530

    mytask

---
+++ src/test/java/org/githubproject/GitClass.java
@@ -0,0 +1,16 @@
+package org.githubproject;
+
+public class GitClass {
+	
+	private void git1() {
+		System.out.println("Initialize hit hub");
+	}
+	public static void main(String[] args) {
+		
+		System.out.println("My git hub program");
+		
+		GitClass gc = new GitClass();
+		gc.git1();
+	}
+
+}
